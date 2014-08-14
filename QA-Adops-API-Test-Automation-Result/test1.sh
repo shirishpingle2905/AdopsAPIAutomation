a7a9d333319e58eddc7379fcb29876f89bd7832e
shirishpingle2905 <shirish@sharethis.com>
2014-08-14 17:12:04 +0530

    changes in shell file

--- QA-Adops-API-Test-Automation-Result/test1.sh
+++ QA-Adops-API-Test-Automation-Result/test1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "hello API"
+echo "API Automation Result Summary."
+
+tail -10 C:/hudson/workspace/QA-Adops-API-Test-Automation/QA-Adops-API-Test-Automation-Result/QAAdopsAPITestAutomation_TestSuite-Summary.txt > C:/hudson/workspace/QA-Adops-API-Test-Automation/QA-Adops-API-Test-Automation-Result/summary.txt
